485e40e86f2e483ba64254be21b3cdbcbc294a6e
Leonardo Parente <lparente@daitan.com>
2022-04-13 15:04:33 -0400

    Add conanfiles

---
+++ test_package/build/878b1b6e2d4cbc82452d014e37c38868584457e8/CMakeFiles/test_package.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lparente/git/crashpad/test_package/test_package.cpp" "/home/lparente/git/crashpad/test_package/build/878b1b6e2d4cbc82452d014e37c38868584457e8/CMakeFiles/test_package.dir/test_package.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lparente/.conan/data/crashpad/cci.20210507/_/_/package/e5e4094f00b4d346b321077904ab6a96c073dbc2/include"
+  "/home/lparente/.conan/data/linux-syscall-support/cci.20200813/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/lparente/.conan/data/libcurl/7.75.0/_/_/package/0871da0308d339b8bf385c9acfcdbbd9953c6d06/include"
+  "/home/lparente/.conan/data/zlib/1.2.11/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/lparente/.conan/data/openssl/1.1.1n/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
